56af9a98de9ee8574f93b9cdb2d75b9f696628d6
upendra <ugrarapuupendra@gmail.com>
2023-03-06 16:28:16 +0530

    update

---
+++ vars.tf
@@ -0,0 +1,48 @@
+variable "instances" {
+  default = {
+    frontend =  {
+        Name = "frontend"
+        type = "t3.micro"
+    }
+    catalogue =  {
+        Name = "catalogue"
+        type = "t3.micro"
+    }
+    user =  {
+        Name = "user"
+        type = "t3.micro"
+    }
+    cart =  {
+        Name = "cart"
+        type = "t3.micro"
+    }
+    mongodb =  {
+        Name = "mongodb"
+        type = "t3.micro"
+    }
+    mysql =  {
+        Name = "mysql"
+        type = "t3.micro"
+    }
+    rabbitmq =  {
+        Name = "rabbitmq"
+        type = "t3.micro"
+    }
+    redis =  {
+        Name = "redis"
+        type = "t3.micro"
+    }
+    shipping =  {
+        Name = "shipping"
+        type = "t3.micro"
+    }
+    payment =  {
+        Name = "payment"
+        type = "t3.micro"
+    }
+    dispatch =  {
+        Name = "dispatch"
+        type = "t3.micro"
+    }
+  }
+}

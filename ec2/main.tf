482e9966c7892c8e392f5394c75745079ea9d96e
upendra <ugrarapuupendra@gmail.com>
2023-03-06 16:47:33 +0530

    up

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,12 +7,12 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
     ami = data.aws_ami.ami.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [var.sg_id]
+    
 
     tags = {
       Name = var.component
        }
-       
+
 provisioner "remote-exec" {
     
     connection {

da16504e6295debaa1c053d58392cf852c18b7d4
upendra <ugrarapuupendra@gmail.com>
2023-03-08 10:52:13 +0530

    up

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,10 +13,14 @@ resource "aws_instance" "ec2" {
       Name = var.component
        }
 
-provisioner "remote-exec" {
+
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
     
     connection {
-      host = self.public_ip
+      host = aws_instance.ec2.public_ip
       user = "root"
       password = "DevOps321"
     }
@@ -27,7 +31,6 @@ provisioner "remote-exec" {
     ]
 }
 }
-
 
 
 

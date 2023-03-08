ba0816e6f2ea42f145aab9812bced38ef782c2de
upendra <ugrarapuupendra@gmail.com>
2023-03-08 12:09:07 +0530

    up

--- ec2/main.tf
+++ ec2/main.tf
@@ -5,8 +5,9 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "ec2" {
-    ami = data.aws_ami.ami.image_id
-    instance_type = var.instance_type
+    ami                    = data.aws_ami.ami.image_id
+    instance_type          = var.instance_type
+    vpc_security_group_ids = [var.sg_id]
     
 
     tags = {
@@ -24,6 +25,7 @@ resource "null_resource" "provisioner" {
       user = "root"
       password = "DevOps321"
     }
+
     inline = [
         "git clone https://github.com/UPendraugrarapu/shell.git",
         "cd shell",
--- vars.tf
+++ vars.tf
@@ -2,47 +2,47 @@ variable "instances" {
   default = {
     frontend =  {
         Name = "frontend"
-        type = "t2.small"
+        type = "t2.micro"
     }
     catalogue =  {
         Name = "catalogue"
-        type = "t2.small"
+        type = "t2.micro"
     }
     user =  {
         Name = "user"
-        type = "t2.small"
+        type = "t2.micro"
     }
     cart =  {
         Name = "cart"
-        type = "t2.small"
+        type = "t2.micro"
     }
     mongodb =  {
         Name = "mongodb"
-        type = "t2.small"
+        type = "t2.micro"
     }
     mysql =  {
         Name = "mysql"
-        type = "t2.small"
+        type = "t2.micro"
     }
     rabbitmq =  {
         Name = "rabbitmq"
-        type = "t2.small"
+        type = "t2.micro"
     }
     redis =  {
         Name = "redis"
-        type = "t2.small"
+        type = "t2.micro"
     }
     shipping =  {
         Name = "shipping"
-        type = "t2.small"
+        type = "t2.micro"
     }
     payment =  {
         Name = "payment"
-        type = "t2.small"
+        type = "t2.micro"
     }
     dispatch =  {
         Name = "dispatch"
-        type = "t2.small"
+        type = "t2.micro"
     }
   }
 }

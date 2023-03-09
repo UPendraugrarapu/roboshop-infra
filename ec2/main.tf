87374e8b0be5003fedda615a72f6831249e908a9
upendra <ugrarapuupendra@gmail.com>
2023-03-09 10:35:35 +0530

    up

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,6 +18,8 @@ resource "aws_instance" "ec2" {
 }
 
 resource "null_resource" "provisioner" {
+  
+  depends_on = [aws_route53_record.record]
   provisioner "remote-exec" {
     
     connection {

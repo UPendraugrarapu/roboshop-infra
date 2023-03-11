08b9776f6bc98d764794f4e7a4f54ba0e97e4557
upendra <ugrarapuupendra@gmail.com>
2023-03-11 18:03:22 +0530

    up

--- vars.tf
+++ vars.tf
@@ -2,51 +2,51 @@ variable "instances" {
   default = {
     frontend =  {
         Name = "frontend"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     catalogue =  {
         Name = "catalogue"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     user =  {
         Name = "user"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     cart =  {
         Name = "cart"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     mongodb =  {
         Name = "mongodb"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     mysql =  {
         Name = "mysql"
-        type = "t2.micro"
+        type = "t3.micro"
         password = "RoboShop@1"
     }
     rabbitmq =  {
         Name = "rabbitmq"
-        type = "t2.micro"
+        type = "t3.micro"
         password = "roboshop123"
     }
     redis =  {
         Name = "redis"
-        type = "t2.micro"
+        type = "t3.micro"
     }
     shipping =  {
         Name = "shipping"
-        type = "t2.micro"
+        type = "t3.micro"
         password = "RoboShop@1"
     }
     payment =  {
         Name = "payment"
-        type = "t2.micro"
+        type = "t3.micro"
         password = "roboshop123"
     }
     dispatch =  {
         Name = "dispatch"
-        type = "t2.micro"
+        type = "t3.micro"
     }
   }
 }

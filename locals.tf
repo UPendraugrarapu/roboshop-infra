5fd6161ba67f9ef965e331af07935b2acd7ea83a
upendra <ugrarapuupendra@gmail.com>
2023-04-14 12:14:33 +0530

    upd

--- locals.tf
+++ locals.tf
@@ -1,4 +1,7 @@
 locals {
   ##private subnets  
-  private_subnets_ids = {for k,v in module.vpc["main"].private_subnets : k => tomap({"id"=v.id, "availability_zone"=v.availability_zone})}
+  #private_subnets_ids = {for k,v in module.vpc["main"].private_subnets : k => tomap({"id"=v.id, "availability_zone"=v.availability_zone})}
+   private_subnets_ids = [for k,v in module.vpc["main"].private_subnets : v.id]
+
+
 }

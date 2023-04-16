d223e4dac2a8f7b7ffe804b512ab6d8cf3293e0f
upendra <ugrarapuupendra@gmail.com>
2023-04-16 09:44:52 +0530

    upd

--- main.tf
+++ main.tf
@@ -14,14 +14,17 @@ module "docdb" {
   source = "git::https://github.com/UPendraugrarapu/tf-module-docdb.git"
   env   = var.env
   tags  = var.tags
-
+  subnet_ids = local.db_subnets_ids
+  
   for_each = var.docdb
   engine = each.value["engine"]
   engine_version = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]  
-  subnet_ids = local.db_subnets_ids
+  skip_final_snapshot = each.value["skip_final_snapshot"] 
+  no_of_instances = each.value["no_of_instances"] 
+  instnace_class = each.value["instnace_class"]
+  
 
 
 }

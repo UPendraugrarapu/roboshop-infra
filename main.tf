0a8efb757f8d9bf814e175a8245fc1f7e35ea74c
upendra <ugrarapuupendra@gmail.com>
2023-04-18 15:56:47 +0530

    upd

--- main.tf
+++ main.tf
@@ -70,3 +70,13 @@ module "rabbitmq" {
   for_each = var.rabbitmq
   instance_type = each.value["instance_type"]
 }
+
+module "alb" {
+  source = "git::https://github.com/UPendraugrarapu/tf-module-alb.git"
+  env   = var.env
+  tags  = var.tags
+  name = each.value["name"]
+  internal = each.value["internal"]
+  load_balancer_type = each.value["load_balancer_type"]
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
+}

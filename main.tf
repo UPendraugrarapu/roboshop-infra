25f8d50e5b4e63a4181916c5df00600dbe6a7ce4
upendra <ugrarapuupendra@gmail.com>
2023-04-20 09:13:52 +0530

    upd

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# module "vpc" {
-#   source = "git::https://github.com/UPendraugrarapu/tf-module-vpc.git"
-#   env   = var.env
-#   tags  = var.tags
-#   default_route_table = var.default_route_table
-#   default_vpc_id = var.default_vpc_id
-#   for_each = var.vpc
-#   vpc_cidr = each.value["vpc_cidr"]
-#   public_subnets = each.value ["public_subnets"]
-#   private_subnets = each.value ["private_subnets"]
-# }
+module "vpc" {
+  source = "git::https://github.com/UPendraugrarapu/tf-module-vpc.git"
+  env   = var.env
+  tags  = var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id = var.default_vpc_id
+  for_each = var.vpc
+  vpc_cidr = each.value["vpc_cidr"]
+  public_subnets = each.value ["public_subnets"]
+  private_subnets = each.value ["private_subnets"]
+}
 
 # module "docdb" {
 #   source = "git::https://github.com/UPendraugrarapu/tf-module-docdb.git"

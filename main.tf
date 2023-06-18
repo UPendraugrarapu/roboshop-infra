5a0e6d4b4e4b2b7d3d0e2c5a50429fc528c6285b
upendra <ugrarapuupendra@gmail.com>
2023-06-18 14:43:36 +0530

    Updated code

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ module "elasticache" {
 }
 
 module "rabbitmq" {
+  depends_on = [module.vpc]
   source = "git::https://github.com/UPendraugrarapu/tf-module-rabbitmq.git"
   env   = var.env
   tags  = var.tags
